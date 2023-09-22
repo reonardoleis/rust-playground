681af2c313a55d1f37200217b60b7a30095e93a0
Leonardo <accs.lndrs@gmail.com>
2023-09-22 09:31:54 -0300

    added borrowing experiences

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
+    let mut s1 = String::from("Hello, ");
+    let mut s2 = foo( &s1);
 
-    
+
+    println!("{} {}", s1, s2);
+
+    s1 = String::from("world!");
+    s2 = foo(&s1);
+
+    println!("{} {}", s1, s2);
+}
+
+fn foo(str: &String) -> &str {
+    return &str[..];
 }
